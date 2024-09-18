d26a962ffa4e9676036053a65eedadb0daf015f9
Camilo Bastos Ribeiro <cb_ambiental@hotmail.com>
2024-09-18 18:12:20 -0300

    Update analysis_E04.py
    inclusion of a function to identify the time periods with the highest emission in each pixel and in each emission sector

--- E04/scripts/analysis_E04.py
+++ E04/scripts/analysis_E04.py
@@ -7,14 +7,11 @@ Created on Mon Sep 16 14:06:38 2024
 
 import xarray as xr
 import numpy as np
-from netCDF4 import Dataset
 import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
 import temporalStatistics as tst
 import os
 
-def emis_freq(dir_folder, var, op, freq='monthly'):
+def aggEmis(dir_folder, var, op, freq='monthly'):
     
     """
     Function to aggregate emissions based on the specified mathematical operation and 
@@ -76,6 +73,8 @@ def emis_freq(dir_folder, var, op, freq='monthly'):
                     pol_2d = pd.DataFrame(
                         op(np.array(data[var][time_indices, 0, :, :]), axis=0).flatten()
                     ).rename(columns={0: sector_name})
+                    max_value = pol_2d.max()
+                    print(f'max value {var} = {max_value}')
                     
                     #Add values for all sectors in the df
                     if t not in final_df:
@@ -92,7 +91,7 @@ def emis_freq(dir_folder, var, op, freq='monthly'):
                 max_value = pol_2d.max()
                 print(f'max value {var} = {max_value}')
                 
-                # Add the values to final df (yealy)
+                # Add the values to final df (yearly)
                 if final_df.empty:
                     final_df = pol_2d
                 else:
@@ -105,6 +104,13 @@ def emis_freq(dir_folder, var, op, freq='monthly'):
     else:
         # Get the major emitter for 'yearly' freq
         final_df['major_emitter'] = final_df.idxmax(axis=1)
+        
+    # Add the total emissions column after identifying the major emitter
+    if freq != 'yearly':
+        for t, df in final_df.items():
+            df['total_emissions'] = df.drop(columns=['major_emitter']).sum(axis=1)
+    else:
+        final_df['total_emissions'] = final_df.drop(columns=['major_emitter']).sum(axis=1)
     
     return final_df
 
@@ -142,3 +148,50 @@ def latlon_2d(dir_data):
     lat2d = ylat.flatten()
     
     return lon2d, lat2d
+
+
+def highTime(dfs, time_labels):
+    """
+    Identifies the time period with the highest emission for each pixel (row) and each sector (column).
+    
+    Parameters:
+    - dfs: list of DataFrames, each corresponding to a different time period (e.g., months, days, hours),
+           where each column represents an emission sector.
+    - time_labels: list of strings representing the labels for each time period, in the same order as the DataFrames.
+    
+    Returns:
+    - A DataFrame where each cell contains the label of the time period with the highest emission for the respective pixel and sector.
+    """
+    if len(dfs) != len(time_labels):
+        raise ValueError("The number of DataFrames must match the number of time labels.")
+
+    # Stack DataFrames along a new axis (adds a 'time' dimension)
+    stacked_data = np.stack([df.to_numpy() for df in dfs], axis=2)
+    
+    # Find the index of the time period with the highest value for each pixel (row) and sector (column)
+    max_indices = np.argmax(stacked_data, axis=2)
+
+    # Create a DataFrame where each cell contains the label of the time period with the highest emission
+    high_time_df = pd.DataFrame(
+        data=np.vectorize(lambda idx: time_labels[idx])(max_indices),
+        columns=dfs[0].columns,
+        index=dfs[0].index
+    )
+
+    return high_time_df
+
+#%%
+
+dir_folder = r'C:\Documentos_Profissionais\LCQAr\projeto_Congonhas_MG\emission_2023\PM10'
+var = 'PM10'
+
+mes_sum = aggEmis(dir_folder, var, np.sum, freq='monthly')
+
+monthly_dfs = [mes_sum[1], mes_sum[2], mes_sum[3], mes_sum[4], mes_sum[5],
+                      mes_sum[6], mes_sum[7], mes_sum[8], mes_sum[9], 
+                      mes_sum[10], mes_sum[11],mes_sum[12]]
+
+time_labels = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+result_df = highTime(monthly_dfs, time_labels)
+print(result_df)
